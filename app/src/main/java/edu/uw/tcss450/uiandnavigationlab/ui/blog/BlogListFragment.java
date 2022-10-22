5fa8fb9d6592c080faafc40c5066b2ffad0635f4
Kevin Kulman <97306295+kdkulman@users.noreply.github.com>
2022-10-22 00:00:18 -0700

    blogs retrieved from api

--- app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/blog/BlogListFragment.java
+++ app/src/main/java/edu/uw/tcss450/uiandnavigationlab/ui/blog/BlogListFragment.java
@@ -2,36 +2,58 @@ package edu.uw.tcss450.uiandnavigationlab.ui.blog;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450.uiandnavigationlab.R;
+import edu.uw.tcss450.uiandnavigationlab.databinding.FragmentBlogListBinding;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link BlogListFragment#newInstance} factory method to
+ * Use the {@link BlogListFragment# newInstance} factory method to
  * create an instance of this fragment.
  */
 public class BlogListFragment extends Fragment {
-
+    private BlogListViewModel mModel;
 
     public BlogListFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mModel = new ViewModelProvider(getActivity()).get(BlogListViewModel.class);
+        mModel.connectGet();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blog_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FragmentBlogListBinding binding = FragmentBlogListBinding.bind(getView());
+
+        mModel.addBlogListObserver(getViewLifecycleOwner(), blogList -> {
+            if (!blogList.isEmpty()) {
+                binding.listRoot.setAdapter(
+                        new BlogRecyclerViewAdapter(blogList)
+                );
+                binding.layoutWait.setVisibility(View.GONE);
+            }
+        });
     }
 }
